c766b9deadbafbf6d68566eecd1917ac81700fe1
e235733 <e235733@ie.u-ryukyu.ac.jp>
2026-05-19 22:33:35 +0900

    feat: network config

--- nn.py
+++ nn.py
@@ -1,160 +1,162 @@
 import numpy as np
-import function as fn
+from common.layers import *
+from collections import OrderedDict
 
-# ニューラルネットワークのモデルクラス
+# ネットワークを設計可能なモデル
 class NN_Model:
-    def __init__(self, input_dim, hidden_layer, output_dim,
-                act_fn:fn.ActivationFunction = fn.LeakyReLU(),
-                output_fn:fn.OutputFunction = fn.Softmax(),
-                eta=0.01, l2_lambda=0.005, alpha=0.9):
+    def __init__(self, input_dim=(1, 28, 28), 
+                 layer_config=[['Conv', 30, 5, 2, 1], ['Relu'], ['Pool', 2, 2, 2], ['Affine', 100], ['Relu'], ['Affine', 10]], 
+                 last_layer='Softmax', weight_init_type='he'):
+
+        self.params = {}
+        self.layers = OrderedDict()
+
+        latest_dim = input_dim
+        param_count = 0
+        layer_count = {'Conv': 0, 'Pool': 0, 'Affine': 0, 'Relu': 0}
         
-        self.input_dim = input_dim
-        self.hidden_layer = hidden_layer
-        self.output_dim = output_dim
-        self.depth = len(hidden_layer)
+        for i, l in enumerate(layer_config):
+            match l[0]:
+                case 'Conv':
+                    filter_num = l[1]
+                    filter_size = l[2]
+                    filter_pad  = l[3]
+                    filter_stride = l[4]
 
-        self.act_fn = act_fn # 隠れ層の活性化関数
-        self.output_fn = output_fn # 出力層の活性化関数
+                    # ノード数から初期値の標準偏差を計算
+                    node_num = latest_dim[0] * filter_size * filter_size
+                    weight_init_std = self._get_weight_init_std(weight_init_type, node_num)
 
-        self.eta = eta # 学習率
-        self.l2_lambda = l2_lambda # L2正則化のペナルティ
-        self.alpha = alpha # 慣性係数
+                    param_count += 1
+                    param_ord = str(param_count)
 
-        # パラメータの初期化
-        self._initialize_parameters()
+                    self.params['W'+param_ord] = weight_init_std * \
+                    np.random.randn(filter_num, latest_dim[0], filter_size, filter_size)
 
-        # グラフ作成用の損失記録
-        self.train_loss_history = [] 
-        self.test_loss_history = []
+                    self.params['b'+param_ord] = np.zeros(filter_num)
 
-    def _initialize_parameters(self):
-        self.W = []
-        self.b = []
+                    layer_count['Conv'] += 1
+                    layer_ord = str(layer_count['Conv'])
+                    self.layers['Conv'+layer_ord] = Convolution(self.params['W'+param_ord], self.params['b'+param_ord], filter_stride, filter_pad)
+
+                    out_h = 1 + int((latest_dim[1] + 2*filter_pad - filter_size) / filter_stride)
+                    out_w = 1 + int((latest_dim[2] + 2*filter_pad - filter_size) / filter_stride)
+                    latest_dim=(filter_num, out_h, out_w)
+
+                case 'Pool':
+                    pool_h = l[1]
+                    pool_w = l[2]
+                    pool_stride = l[3]
+
+                    layer_count['Pool'] += 1
+                    layer_ord = str(layer_count['Pool'])
+                    self.layers['Pool'+layer_ord] = Pooling(pool_h, pool_w, pool_stride)
+
+                    out_h = int(1 + (latest_dim[1] - pool_h) / pool_stride)
+                    out_w = int(1 + (latest_dim[2] - pool_w) / pool_stride)
+                    latest_dim = (latest_dim[0], out_h, out_w)
+
+                case 'Affine':
+                    input_size = None
+                    if (len(latest_dim) == 3):
+                        input_size = latest_dim[0] * latest_dim[1] * latest_dim[2]
+                    elif (len(latest_dim) == 1):
+                        input_size = latest_dim[0]
+                    output_size = l[1]
+
+                    # ノード数から初期値の標準偏差を計算
+                    weight_init_std = self._get_weight_init_std(weight_init_type, input_size)
+
+                    param_count += 1
+                    param_ord = str(param_count)
+
+                    self.params['W'+param_ord] = weight_init_std * \
+                    np.random.randn(input_size, output_size)
+                    self.params['b'+param_ord] = np.zeros(output_size)
+
+                    layer_count['Affine'] += 1
+                    layer_ord = str(layer_count['Affine'])
+                    self.layers['Affine'+layer_ord] = Affine(self.params['W'+param_ord], self.params['b'+param_ord])
+
+                    latest_dim = (output_size, )
+
+                case 'Relu':
+                    layer_count['Relu'] += 1
+                    layer_ord = str(layer_count['Relu'])
+                    self.layers['Relu'+layer_ord] = Relu()
+
+        match last_layer:
+            case 'Softmax':
+                self.last_layer = SoftmaxWithLoss()
+
+    def _get_weight_init_std(self, weight_init_type, node_num):
+        """重みの標準偏差を計算する"""
+        if str(weight_init_type).lower() in ('relu', 'he'):
+            return np.sqrt(2.0 / node_num)
+        elif str(weight_init_type).lower() in ('sigmoid', 'xavier'):
+            return np.sqrt(1.0 / node_num)
         
-        rng = np.random.default_rng()
+        # 数値指定（固定値）の場合
+        try:
+            return float(weight_init_type)
+        except (ValueError, TypeError):
+            return 0.01
+
+    def predict(self, x):
+        """順伝播による予測
+        x: 入力データ
+        """
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
+
+    def loss(self, x, t):
+        """損失関数を求める
+        x: 予測データ
+        t: 正解ラベル
+        """
+        y = self.predict(x)
+        return self.last_layer.forward(y, t)
+
+    def accuracy(self, x, t):
+        """正解率を求める
+        x: 予測データ
+        t: 正解ラベル
+        batch_size: 正解率計算のバッチサイズ
+        """
+        if t.ndim != 1:
+            t = np.argmax(t, axis=1)
+
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        num_correct = np.sum(y == t)
         
-        layers = [self.input_dim] + self.hidden_layer + [self.output_dim]
-        
-        for i in range(len(layers) - 1):
-            head = layers[i]
-            tail = layers[i+1]
-            
-            # 活性化関数に基づいた初期化スケールを取得
-            scale = self.act_fn.init_weight(head, tail)
-            
-            w = rng.standard_normal((head, tail)) * scale
-            b = np.zeros(tail)
-            
-            self.W.append(w)
-            self.b.append(b)
+        return num_correct / x.shape[0]
 
-        # Momentum 用の速度 V をゼロ初期化
-        self.V_w = [np.zeros_like(w) for w in self.W]
-        self.V_b = [np.zeros_like(b) for b in self.b]
-    
-    def calc_forward_propagation(self, X):
-        # 前向き伝播
-        L = self.depth
-        self.A = [X]
-        for i in range(L):
-            Z = self.A[i] @ self.W[i] + self.b[i]
-            self.A.append(self.act_fn.value(Z))
-        Z = self.A[L] @ self.W[L] + self.b[L]
-        self.P = self.output_fn.value(Z)
+    def gradient(self, x, t):
+        """勾配を求める
+        x : 入力データ
+        t : 教師ラベル
+        """
+        # forward
+        self.loss(x, t)
 
-    def append_grad(self, i, dz, threshold=5.0):
-        # i 番目の dw, db を dz から計算
-        dw = self.A[i].T @ dz + self.l2_lambda * self.W[i]
-        dw_clipped = np.clip(dw, -threshold, threshold)
-        db = np.sum(dz, axis=0)
-        
-        self.dW.append(dw_clipped)
-        self.db.append(db)
+        # backward
+        dout = 1
+        dout = self.last_layer.backward(dout)
 
-    def calc_backward_propagation(self, Y):
-        # 逆向き伝播
-        self.dW = []
-        self.db = []
-        dz = self.output_fn.dLoss(self.P, Y)
-        self.append_grad(self.depth, dz)
-        for i in range(self.depth, 0, -1):
-            da_prev = dz @ self.W[i].T
-            dz = da_prev * self.act_fn.diff(self.A[i])
-            self.append_grad(i-1, dz)
-        self.dW.reverse()
-        self.db.reverse()
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
 
-    def update_parameters(self):
-        # パラメータの更新
-        for i in range(self.depth + 1):
-            self.V_w[i] = self.alpha * self.V_w[i] - self.eta * self.dW[i]
-            self.V_b[i] = self.alpha * self.V_b[i] - self.eta * self.db[i]
-            self.W[i] += self.V_w[i]
-            self.b[i] += self.V_b[i]
+        # 設定
+        grads = {}
+        param_count = 1
+        for layer in self.layers.values():
+            if hasattr(layer, 'dW'):
+                grads['W' + str(param_count)] = layer.dW
+                grads['b' + str(param_count)] = layer.db
+                param_count += 1
 
-    def shift(self, X, Y):
-        # 勾配を計算
-        self.calc_forward_propagation(X)
-        self.calc_backward_propagation(Y)
-        # パラメータを更新
-        self.update_parameters()
-
-    def predict(self, X):
-        A = X       
-        for i in range(self.depth):
-            Z = A @ self.W[i] + self.b[i]
-            A = self.act_fn.value(Z)
-        i = self.depth
-        Z = A @ self.W[i] + self.b[i]
-        return self.output_fn.value(Z)
-    
-    def loss(self, X, Y):
-        P = self.predict(X)
-        base_loss = self.output_fn.Loss(P, Y)
-        
-        l2_penalty = 0.0
-        for w in self.W:
-            l2_penalty += np.sum(w ** 2) 
-        l2_penalty *= (self.l2_lambda / 2)
-
-        return l2_penalty + base_loss
-    
-    def log_train_loss(self, X_train, Y_train):
-        train_loss = self.loss(X_train, Y_train)
-        self.train_loss_history.append(train_loss)
-        return train_loss
-
-    def log_test_loss(self, X_test, Y_test):
-        test_loss = self.loss(X_test, Y_test)
-        self.test_loss_history.append(test_loss)
-        return test_loss
-
-    def evaluate_accuracy(self, X, Y: np.ndarray):
-        # 予測と正解を比較して精度を計算
-        predicted_classes = np.argmax(self.predict(X), axis=1)
-        Y_labels = np.argmax(Y, axis=1) if Y.ndim > 1 else Y
-        accuracy = np.mean(predicted_classes == Y_labels)
-        return accuracy
-
-    
-if __name__ == "__main__":
-    import function as fn
-
-    # 簡易テスト
-    X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    Y = np.array([[1, 0], [0, 1], [0, 1], [1, 0]]) # XOR っぽいラベル
-
-    model = NN_Model(
-        input_dim=2,
-        hidden_layer=[4, 4],
-        output_dim=2,
-        act_fn=fn.Sigmoid(),
-        output_fn=fn.Softmax(),
-        eta=0.1
-    )
-
-    print("Initial Loss:", model.loss(X, Y))
-    for _ in range(100):
-        model.shift(X, Y)
-    print("Loss after 100 steps:", model.loss(X, Y))
-    print("Predictions:\n", model.predict(X))
+        return grads
