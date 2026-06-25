f7319bf486946a26fa76936db32e9b6bda8d7e59
e235733 <e235733@ie.u-ryukyu.ac.jp>
2026-06-25 19:19:24 +0900

    feat: implement Adam optimizer
    Add Adam with bias-corrected moment estimates, following the same
    style and weight_decay (L2 on 'W' params) support as the existing
    SGD/Momentum/RMSProp optimizers. Register 'adam' in Trainer's
    optimizer_class_dict so it can be selected.

--- common/optimizer.py
+++ common/optimizer.py
@@ -54,3 +54,33 @@ class RMSProp:
                     grads[key] += self.weight_decay * params[key]
                 self.h[key] = self.alpha * self.h[key] + (1.0 - self.alpha) * grads[key] ** 2
                 params[key] -= self.lr * grads[key] / (xp.sqrt(self.h[key]) + 1e-7)
+
+
+class Adam:
+    def __init__(self, lr=0.001, beta1=0.9, beta2=0.999, weight_decay=0):
+        self.lr = lr
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.weight_decay = weight_decay
+        self.iter = 0
+        self.m = None
+        self.v = None
+
+    def update(self, params, grads):
+        if self.m is None:
+            self.m = {}
+            self.v = {}
+            for key, val in params.items():
+                self.m[key] = xp.zeros_like(val)
+                self.v[key] = xp.zeros_like(val)
+
+        self.iter += 1
+        lr_t = self.lr * xp.sqrt(1.0 - self.beta2 ** self.iter) / (1.0 - self.beta1 ** self.iter)
+
+        for key in params.keys():
+            if key in grads:
+                if self.weight_decay > 0 and 'W' in key:
+                    grads[key] += self.weight_decay * params[key]
+                self.m[key] += (1 - self.beta1) * (grads[key] - self.m[key])
+                self.v[key] += (1 - self.beta2) * (grads[key] ** 2 - self.v[key])
+                params[key] -= lr_t * self.m[key] / (xp.sqrt(self.v[key]) + 1e-7)
